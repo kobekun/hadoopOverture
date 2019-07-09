41925c4645a85b7da8639d68b801c2b99b17207f
kobekun <kobekun881105@126.com>
2019-07-09 22:24:51 +0800

    本地词频统计提交MapReduce作业

---
+++ src/main/java/com/kobekun/hadoop/mapreduce/wc/WordCountLocalApp.java
@@ -0,0 +1,59 @@
+package com.kobekun.hadoop.mapreduce.wc;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * 使用MR统计hdfs上的文件对应的词频
+ *
+ * Driver： 配置Mapper，Reducer的相关属性
+ *
+ * 提交到本地运行
+ */
+public class WordCountLocalApp {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+
+        //创建任务
+        Job job = Job.getInstance(conf);
+
+        //设置Job对应的参数：主类
+        job.setJarByClass(WordCountLocalApp.class);
+
+        //设置job对应的mapper和reducer处理类
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        //设置Job对应的参数：Mapper的输出key和value的类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        //设置Job对应的参数：reducer的输出key和value的类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        //如果输出目录已经存在则先删除
+//        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.137.2:8020"),conf,"hadoop");
+//        Path output = new Path("/wordcount/kobekun/output");
+//        if(fs.exists(output)){
+//            fs.delete(output, true);
+//        }
+
+        //设置job的参数：作业输入和输出的路径
+        FileInputFormat.setInputPaths(job, new Path("input/wc.input"));
+        FileOutputFormat.setOutputPath(job,new Path("output"));
+
+        //提交作业
+        boolean result = job.waitForCompletion(true);
+
+        System.exit(result ? 0 : -1);
+
+    }
+}
