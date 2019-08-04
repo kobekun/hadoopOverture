5363e805918c4ad8c7d000491395f1056737d1f7
kobekun <kobekun881105@126.com>
2019-08-04 22:18:21 +0800

    页面统计v2

---
+++ src/main/java/com/kobekun/hadoop/userBehaviorLog/mrv2/PagePVstatV2.java
@@ -0,0 +1,85 @@
+package com.kobekun.hadoop.userBehaviorLog.mrv2;
+
+import com.kobekun.hadoop.userBehaviorLog.utils.GetPageId;
+import com.kobekun.hadoop.userBehaviorLog.utils.LogParser;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class PagePVstatV2 {
+
+    public static void main(String[] args) throws Exception{
+
+        Configuration conf = new Configuration();
+
+        FileSystem fileSystem = FileSystem.get(conf);
+
+        Path outputpath = new Path("output/v2/pagepvstat");
+        if(fileSystem.exists(outputpath)){
+            fileSystem.delete(outputpath,true);
+        }
+        Job job = Job.getInstance(conf);
+
+
+        job.setJarByClass(PagePVstatV2.class);
+
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileInputFormat.setInputPaths(job,new Path("input/etl"));
+        FileOutputFormat.setOutputPath(job,new Path("output/v2/pagepvstat"));
+
+        job.waitForCompletion(true);
+    }
+
+    static class MyMapper extends Mapper<LongWritable,Text, Text,LongWritable> {
+
+        private LogParser logParser;
+        private LongWritable ONE = new LongWritable(1);
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+
+            logParser = new LogParser();
+        }
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+            String log = value.toString();
+            Map<String,String> logInfo = logParser.parse2(log);
+
+            String pageId = GetPageId.getPageId(logInfo.get("url"));
+            context.write(new Text(pageId), ONE);
+        }
+    }
+
+    static class MyReducer extends Reducer<Text,LongWritable,Text,LongWritable> {
+
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+
+            long count = 0;
+            for(LongWritable access : values){
+                count++;
+            }
+            context.write(key, new LongWritable(count));
+        }
+    }
+}
